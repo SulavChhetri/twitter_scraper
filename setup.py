6ef158ef0765b217fe02b4afbebc6c253c2eb503
Xulav <sulavthapachhetri@gmail.com>
2022-10-30 23:15:27 +0545

    start

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(
+    name="twitterscraper",
+    version="1.0.0",
+    description="This is a twitter scraper package which can scrape the name,username,location and date of birth of the username you search.",
+    author="Sulav",
+    packages=['src'],
+    install_requrires=[]
+    )
